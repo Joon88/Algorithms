dcc487578551d7d3e0d73cefa3b1ba53ee1d61e9
Joon88 <joon.oracle@gmail.com>
2025-03-01 12:54:44 +0530

    interview prep

--- StacksAndQueues/src/MaxInStack.java
+++ StacksAndQueues/src/MaxInStack.java
@@ -1,10 +1,11 @@
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class MaxInStack {
 	// MyStack stack = new MyStack();
 	public void solve() {
-		MyStack stack = new MyStack();
+		MyStack1 stack = new MyStack1();
 		stack.push(2);
 		stack.push(4);
 		stack.push(4);
@@ -12,7 +13,49 @@ public class MaxInStack {
 		stack.push(3);
 		stack.push(4);
 		stack.push(5);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
 		System.out.println(stack.getMax());
+	}
+
+	public class MyStack1 {
+		private static Stack<Integer> stack = new Stack<>();
+		private static int max = Integer.MIN_VALUE;
+
+		public void push(int x) {
+			if(stack.isEmpty()) {
+				stack.push(x);
+				max = x;
+			} else if(x <= max) {
+				stack.push(x);
+			} else {
+				// max is changing
+				int val = max + x;
+				stack.push(val);
+				max = x;
+			}
+		}
+
+		public Integer pop() {
+			if (stack.isEmpty())
+				return null;
+			
+			if (max >= stack.peek())
+				return stack.pop();
+			
+			int oldMax = max;
+			int newMax = stack.pop() - oldMax;
+			max = newMax;
+			return oldMax;
+		}
+
+		public Integer getMax() {
+			return max;
+		}
 	}
 
 	public class MyStack {
